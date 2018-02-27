8ea23d024eb0e3db89e8c420a40fa375041e3820
fzheng <fzheng@thoughtworks.com>
2018-02-27 15:33:02 +0800

    refactor: move prepareData to page and keep catalogue simple

--- src/components/catalogue/catalogue.js
+++ src/components/catalogue/catalogue.js
@@ -1,77 +1,21 @@
 Component({
   properties: {
     categories: Array,
-    items: Array
+    categorizedItems: Array
   },
 
-  attached: function(){
-    this._setGroupedItems();
+  data: {
+    toViewId: ''
   },
 
   methods: {
 
-    _setGroupedItems: function () {
-      this.setData({
-          groupedItems: convertToGroupedItems(this.properties.items)
-      });
-    },
-
     navToGroup: function (event) {
-      const name = event.target.dataset.name.toLowerCase();
-      const groupName = generateGroupName(name);
-      let id = generateGroupId(groupName);
+      const id = event.target.dataset.categoryid;
       this.setData({
           toViewId: id
       })
     }
-  },
-
-  data: {
-    groupedItems: [],
-    toViewId: ''
   }
 
 })
-
-function convertToGroupedItems (items) {
-    let grouped = {};
-
-    items.forEach(function (item){
-        let char = item.name.charAt(0).toLowerCase();
-        let groupName = generateGroupName(char);
-        grouped[groupName] = grouped[groupName] || [];
-        grouped[groupName].push(item);
-    });
-
-    let groups = Object.keys(grouped).sort();
-    if (groups[0] === '#') {
-        groups.shift();
-        groups.push('#');
-    }
-
-    const groupedItems = groups.map((groupName) => {
-      return {
-          groupId: generateGroupId(groupName),
-          groupName: groupName,
-          items: grouped[groupName]
-      }
-    });
-
-    return groupedItems;
-}
-
-function generateGroupName(firstLetter) {
-    let charCode = firstLetter.charCodeAt(0);
-    if (charCode >= 'a'.charCodeAt(0) && charCode <= 'z'.charCodeAt(0)) {
-      return firstLetter;
-    } else {
-        return '#'
-    }
-}
-
-function generateGroupId(groupName) {
-    if (groupName === '#') {
-        return 'special_group'
-    }
-    return groupName;
-}
--- src/pages/catalogue/brands.js
+++ src/pages/catalogue/brands.js
@@ -231,26 +231,63 @@ const brands = [
 Page({
 
   data: {
-    items: brands,
+    categorizedItems: brands,
     categories: []
   },
 
   onLoad() {
-    this._generateAlphabet();
-  },
-
-  _generateAlphabet: function () {
-
-      let alphabet = [];
-      for (let i = 'A'.charCodeAt(0); i <= 'Z'.charCodeAt(0); i++) {
-          alphabet.push({name: String.fromCharCode(i), text: String.fromCharCode(i)});
+    this.setData(
+      {
+        categories: generateAlphabet(),
+        categorizedItems: convertToCategorizedItems(brands)
       }
-
-      alphabet.push({name:'#', text: '#'});
-
-      this.setData({
-        categories: alphabet
-      });
+    );
   }
 
 })
+
+const generateAlphabet = () => {
+
+    let alphabet = [];
+    for (let i = 'A'.charCodeAt(0); i <= 'Z'.charCodeAt(0); i++) {
+        alphabet.push({id: String.fromCharCode(i), name: String.fromCharCode(i)});
+    }
+
+    alphabet.push({id:'#', name: '#'});
+    return alphabet;
+};
+
+const generateGroupName = (firstLetter) => {
+    let charCode = firstLetter.charCodeAt(0);
+    if (charCode >= 'A'.charCodeAt(0) && charCode <= 'Z'.charCodeAt(0)) {
+      return firstLetter;
+    } else {
+        return '#'
+    }
+};
+
+const convertToCategorizedItems = (items) => {
+  let groups = {};
+
+  items.forEach(function (item){
+      let char = item.name.charAt(0).toUpperCase();
+      let groupName = generateGroupName(char);
+      groups[groupName] = groups[groupName] || [];
+      groups[groupName].push(item);
+  });
+
+  let groupsNames = Object.keys(groups).sort();
+  if (groupsNames[0] === '#') {
+      groupsNames.shift();
+      groupsNames.push('#');
+  }
+
+  const categorizedItems = groupsNames.map((groupName) => {
+    return {
+        categoryId: groupName,
+        categoryName: groupName,
+        items: groups[groupName]
+    }
+  });
+  return categorizedItems;
+};
