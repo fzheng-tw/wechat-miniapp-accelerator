16cfcb901aa1789f19d5af3b7ff91b335c19551c
fzheng <fzheng@thoughtworks.com>
2018-03-01 16:58:29 +0800

    add product detail component

---
+++ src/pages/productDetail/productDetail.js
@@ -0,0 +1,35 @@
+const product = {
+  images: [
+    'https://www.celine.com/dw/image/v2/BBST_PRD/on/demandware.static/-/Sites-masterCatalog/default/dw532aec5d/images/large/185333A8V.38NO_1_SUM18_50479.jpg?sw=1156&sh=1600&sm=fit&strip=false',
+    'https://www.celine.com/dw/image/v2/BBST_PRD/on/demandware.static/-/Sites-masterCatalog/default/dw19964629/images/large/185333A8V.38NO_2_SUM18_51807.jpg?sw=1156&sh=1600&sm=fit&strip=false',
+    'https://www.celine.com/dw/image/v2/BBST_PRD/on/demandware.static/-/Sites-masterCatalog/default/dw4801cec5/images/large/185333A8V.38NO_4_SUM18_50481.jpg?sw=1156&sh=1600&sm=fit&strip=false',
+    'https://www.celine.com/dw/image/v2/BBST_PRD/on/demandware.static/-/Sites-masterCatalog/default/dwf01679ab/images/large/185333A8V.38NO_3_SUM18_56655.jpg?sw=1156&sh=1600&sm=fit&strip=false'
+
+  ],
+  brand: {
+    english_name: 'Céline',
+    chinese_name: '赛琳'
+  },
+  english_name: 'PURSE CLUTCH IN SMOOTH CALFSKIN',
+  chinese_name: 'PURSE光滑小牛皮手包',
+  price: '26,500HKD',
+  description: [
+    "手提",
+    "黄铜锁扣",
+    "3个内部隔层及拉链袋",
+    "3个内部隔层及拉链袋",
+    "13 x 8 x 3英寸(32 x 20 x 8厘米)",
+    "金色金属件",
+    "100%小牛皮",
+    "100%小羊皮衬里",
+    "编号：185333A8V.38NO"
+  ]
+};
+
+Page({
+
+  data: {
+    product: product
+  }
+
+})
